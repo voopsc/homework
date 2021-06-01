b4fb11fd8eef2d76d1553b3a3b3353dddab0c47f
voopsc <71984988+voopsc@users.noreply.github.com>
2021-06-01 17:19:04 +0300

    update(autoload): fix autoload and create autoload class with params

--- index.php
+++ index.php
@@ -10,28 +10,23 @@
     use Framework\Router as Router;
 
     // Autoloader
-    // $autoloadPath = [FRAMEWORK, 'Autoload', 'Autoload.php'];
-    // require_once(implode(DIRECTORY_SEPARATOR, $autoloadPath));
+    $autoloadPath = [FRAMEWORK, 'Autoload', 'Autoload.php'];
+    require_once(implode(DIRECTORY_SEPARATOR, $autoloadPath));
 
     $autoload = new Autoloader\Autoload([
-      'Framework\\Router' => 'Framework/Router',
+      'Framework\\Router' => 'Framework'.DIRECTORY_SEPARATOR.'Router',
+      'Framework\\Autoload' => 'Framework'.DIRECTORY_SEPARATOR.'Autoload',
     ]);
-    
+
     $autoload->register();
 
-    // $a = scandir(ROOT);
-    // print_r($a);
-
-
-
-    // $autoload = new Autoload;
-    // $autoload->register();
 
     // Router
-    // FIXME: Will fix by autoloader next time - its a temporary descicion
-    $routerPath = [FRAMEWORK, 'Router', 'Router.php'];
-    $routes = (implode(DIRECTORY_SEPARATOR, [ROOT, 'App', 'config', 'routes.php']));
-    require_once(implode(DIRECTORY_SEPARATOR, $routerPath));
-
+    $routes = (implode(DIRECTORY_SEPARATOR, [
+      ROOT,
+      'App',
+      'config',
+      'routes.php'])
+    );
     $router = new Router\Router($routes);
     $router->run();
