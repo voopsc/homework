cfc129bd91917491ac2b203809178706bbc82c2e
voopsc <voopstudio@gmail.com>
2021-06-09 00:23:34 +0300

    create(files): create view files and controller

---
+++ App/view/layout/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="uk-ua" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1'/>
+    <meta content='true' name='HandheldFriendly'/>
+    <meta content='width' name='MobileOptimized'/>
+    <meta content='yes' name='apple-mobile-web-app-capable'/>
+    <title>Мій магазин</title>
+    <!-- Font -->
+    <link rel="stylesheet" href="/App/html/css/font.css">
+    <!-- Style -->
+    <link rel="stylesheet" href="/App/html/css/reset.css">
+    <link rel="stylesheet" href="/App/html/css/style.css">
+  </head>
+
+  <body>
