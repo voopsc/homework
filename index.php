fe97b2e1401b2e704d8cff8c6083694092a80f1c
voopsc <voopstudio@gmail.com>
2021-06-14 21:49:12 +0300

    update(feature/composer): little fixes of index file

--- index.php
+++ index.php
@@ -6,29 +6,21 @@
     define('FRAMEWORK', ROOT . DIRECTORY_SEPARATOR . 'Framework');
 
     // Namespaces
-    use Framework\Autoload as Autoloader;
-    use Framework\Router as Router;
-    use Framework\Session as Session;
+    use Framework\Router\Router;
+    use Framework\Session\Session;
 
 
     // Autoloader
     $autoloadPath = [ROOT, 'vendor', "autoload.php"];
     require_once(implode(DIRECTORY_SEPARATOR, $autoloadPath));
 
-    // $autoload = new Autoloader\Autoload([
-    //   // 'App\\Model' => 'App'.DIRECTORY_SEPARATOR.'model',
-    // ]);
-
-    // $autoload->register();
-
     // Session
-    $session = new Session\Session;
+    $session = new Session;
     $session->start();
-
 
     // Router
     $routes = Helper::getFilepathString([ROOT, 'App', 'config', 'routes.php']);
     $controller = Helper::getFilepathString([ROOT, 'App', 'controller']);
 
-    $router = new Router\Router($routes, $controller);
+    $router = new Router($routes, $controller);
     $router->run();
