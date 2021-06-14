95218c5c6e38fb510ea3be08778b6bae511ffeb4
voopsc <voopstudio@gmail.com>
2021-06-14 23:19:51 +0300

    update(composer task): add linter and csfixer, update code by fixer

--- Framework/Autoload/Autoload.php
+++ Framework/Autoload/Autoload.php
@@ -1,131 +1,131 @@
 <?php
 
-
     namespace Framework\Autoload;
 
     /** Framework autoload class
      *
      */
-    class Autoload
+class Autoload
+{
+    /**
+    * An associative array where the key is a namespace prefix and the value
+    * is an array of base directories for classes in that namespace.
+    *
+    *@var array $prefixes
+    */
+    protected $prefixes = [];
+
+    /* Constructor
+    *
+    */
+    public function __construct($prefixes = [])
     {
-      /**
-      * An associative array where the key is a namespace prefix and the value
-      * is an array of base directories for classes in that namespace.
-      *
-      *@var array $prefixes
-      */
-      protected $prefixes = [];
-
-      /* Constructor
-      *
-      */
-      public function __construct($prefixes = [])
-      {
         $this->prefixes = $prefixes;
-      }
+    }
 
-      /** Call autoloader functions
-      */
-      public function register()
-      {
+    /** Call autoloader functions
+    */
+    public function register()
+    {
         spl_autoload_register([$this, 'simpleClass']);
         spl_autoload_register([$this, 'defaultLoader']);
         spl_autoload_register([$this, 'autoloader']);
-      }
+    }
 
-      /** Check if class have namespace if yes return array with namespace and
-      * class name
-      * @param string $className full class name with namespace if it exist
-      * @return array
-      */
-      private function getNamespace($className)
-      {
+    /**
+    * Check if class have namespace if yes return array with namespace and
+    * class name
+    * @param string $className full class name with namespace if it exist
+    * @return array
+    */
+    private function getNamespace($className)
+    {
         $result = false;
         if (!empty($className)) {
-          $result = explode('\\', $className);
-          $class = array_pop($result);
-          $result = [
+            $result = explode('\\', $className);
+            $class = array_pop($result);
+            $result = [
             implode(DIRECTORY_SEPARATOR, $result),
             $class,
-          ];
+            ];
         }
 
         return $result;
-      }
+    }
 
-      /** Load class without namespace
-      * @param string $class
-      * @return void
-      */
-      public function simpleClass($class)
-      {
+    /** Load class without namespace
+    * @param string $class
+    * @return void
+    */
+    public function simpleClass($class)
+    {
         $dirPath = ROOT . DIRECTORY_SEPARATOR . 'Framework';
         $scannedDir = array_diff(scandir($dirPath), array('..', '.'));
 
         foreach ($scannedDir as $dir) {
-          $filepath = $dirPath . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $class . '.php';
-          if (file_exists($filepath)) {
-            require_once($filepath);
-            return;
-          }
+            $filepath = $dirPath . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $class . '.php';
+            if (file_exists($filepath)) {
+                require_once($filepath);
+                return;
+            }
         }
-      }
+    }
 
-      /** Load all classes from framework dir
-      * @param string
-      * @return void
-      */
-      public function defaultLoader($class)
-      {
+    /** Load all classes from framework dir
+    * @param string
+    * @return void
+    */
+    public function defaultLoader($class)
+    {
         $classInfo = $this->getNamespace($class);
         $classInfo = implode(DIRECTORY_SEPARATOR, $classInfo);
         $this->requireFile($classInfo . '.php');
-      }
+    }
 
-      /** Load mapped class from class inicialization (constructor)
-      * @param string $className
-      * @return void
-      */
-      public function autoloader($className)
-      {
+    /** Load mapped class from class inicialization (constructor)
+    * @param string $className
+    * @return void
+    */
+    public function autoloader($className)
+    {
         $classData = $this->getNamespace($className);
 
         foreach ($this->prefixes as $namespace => $filepath) {
-          if (preg_match("~[^$namespace$]~", $classData[0])) {
-            $this->requireFile($filepath . '/' . $classData[1] . '.php');
-            return;
-          }
+            if (preg_match("~[^$namespace$]~", $classData[0])) {
+                $this->requireFile($filepath . '/' . $classData[1] . '.php');
+                return;
+            }
         }
-      }
+    }
 
-      /** Load single class by classname
-      * @param string $className
-      */
-      public function load($className)
-      {
+    /** Load single class by classname
+    * @param string $className
+    */
+    public function load($className)
+    {
         foreach ($this->prefixes as $namespace => $path) {
-          if (preg_match("~[$namespace]~", $className)) {
-            $filepath = str_replace("$namespace", $path, $className);
-            break;
-          }
+            if (preg_match("~[$namespace]~", $className)) {
+                $filepath = str_replace("$namespace", $path, $className);
+                break;
+            }
         }
         echo $filepath;
         $result = $this->requireFile($filepath.'.php');
-      }
-
-      /**
-       * If a file exists, require it from the file system.
-       * @param string $file The file to require.
-       * @return bool True if the file exists, false if not.
-       */
-      protected function requireFile($file)
-      {
-          if (file_exists($file)) {
-              require_once ROOT . DIRECTORY_SEPARATOR . $file;
-              return true;
-          }
-          // return false;
-      }
-
-      // end of class
     }
+
+    /**
+     * If a file exists, require it from the file system.
+     * @param string $file The file to require.
+     * @return bool True if the file exists, false if not.
+     */
+    protected function requireFile($file)
+    {
+        if (file_exists($file)) {
+            require_once ROOT . DIRECTORY_SEPARATOR . $file;
+            return true;
+        }
+        // return false;
+    }
+
+    // end of class
+}
--- Framework/Router/ErrorHandler.php
+++ Framework/Router/ErrorHandler.php
@@ -2,49 +2,60 @@
 
     namespace Framework\Router;
 
-    /** Error handler class for
-     *
-     */
-    class ErrorHandler
+/** Error handler class for
+*
+*/
+class ErrorHandler
+{
+    public function __construct()
     {
-
-      function __construct()
-      {
         $this->developerMode = (int) ini_get('display_errors');
-      }
-
-      public function getError($errorNum)
-      {
-        if ($this->developerMode == 1) {
-          switch ($errorNum) {
-            case 5: echo "Error x0006: there is no page (action) controller"; die;
-            case 4: echo "Error x0005: there is no controller file in app or in framework"; die;
-            case 3: echo "Error x0004: there is empty router rules"; die;
-            case 2: echo "Error x0003: there is no action in existed controller"; die;
-            case 1: echo "Error x0002: there is no controller file for this URI"; die;
-            case 0: echo "Error x0001: there is no route rule and controller/action for this URI"; die;
-          }
-        } else {
-          $this->serverError();
-        }
-      }
-
-      private function serverError()
-      {
-        die('Some error');
-      }
-
-
-      // FIXME: change get error method from if to exceptions in future
-      // public function register()
-      // {
-      //   set_error_handler([$this, 'getError']);
-      // }
-      //
-      // private function getError($errno, $errstr, $errfile, $errline)
-      // {
-      //
-      //   return true;
-      // }
-      // end of class
     }
+
+    public function getError($errorNum)
+    {
+        if ($this->developerMode == 1) {
+            switch ($errorNum) {
+                case 5:
+                        echo "Error x0006: there is no page (action) controller";
+                    die;
+                case 4:
+                        echo "Error x0005: there is no controller file in app or in framework";
+                    die;
+                case 3:
+                        echo "Error x0004: there is empty router rules";
+                    die;
+                case 2:
+                        echo "Error x0003: there is no action in existed controller";
+                    die;
+                case 1:
+                        echo "Error x0002: there is no controller file for this URI";
+                    die;
+                case 0:
+                        echo "Error x0001: there is no route rule and controller/action for this URI";
+                    die;
+            }
+        } else {
+            $this->serverError();
+        }
+    }
+
+    private function serverError()
+    {
+        die('Some error');
+    }
+
+
+    // FIXME: change get error method from if to exceptions in future
+  // public function register()
+  // {
+  //   set_error_handler([$this, 'getError']);
+  // }
+  //
+  // private function getError($errno, $errstr, $errfile, $errline)
+  // {
+  //
+  //   return true;
+  // }
+  // end of class
+}
