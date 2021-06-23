fe4295e2ba167738866b5846d54eea861f4fe3f1
voopsc <voopstudio@gmail.com>
2021-06-24 00:21:27 +0300

    update(Helper): codestyle fixes

--- Framework/Helper/Helper.php
+++ Framework/Helper/Helper.php
@@ -1,44 +1,56 @@
 <?php
 
-    /**
-     *
-     */
+declare(strict_types=1);
+
+namespace Framework\Helper;
+
+/**
+ *
+ */
 class Helper
 {
-  /** Create filepath string from array by implode function
-  * @param array $pathArr array with path "steps"
-  * @return string|void
-  */
-    public static function getFilepathString(array $pathArr)
+    /**
+     * Create filepath string from array by implode function
+     *
+     * @param array $pathArr array with path "steps"
+     *
+     * @return string|null
+     */
+    public static function getFilepathString(array $pathArr): ?string
     {
         if (!empty($pathArr)) {
-            $path = (implode(DIRECTORY_SEPARATOR, $pathArr));
-            return $path;
+            return (implode(DIRECTORY_SEPARATOR, $pathArr));
         }
+        return null;
     }
 
 
-    /** Require once a file by path
-    * @param array $pathArr array with directories to a file
-    * @return void
-    */
+    /**
+     * Require once a file by path
+     *
+     * @param array $pathArr array with directories to a file
+     *
+     * @return void
+     */
     public static function requireFile(array $pathArr)
     {
         $filepath = self::getFilepathString($pathArr);
 
         if (file_exists($filepath)) {
-            return require_once($filepath);
+            require_once($filepath);
         }
     }
 
 
-    /** Return link of prev page by http_ref
-    * @return string
-    */
-    public static function getRefererURI()
+    /**
+     * Return link of prev page by http_ref
+     *
+     * @return string
+     */
+    public static function getRefererURI(): string
     {
         if (isset($_SERVER['HTTP_REFERER'])) {
-            return (string) $_SERVER['HTTP_REFERER'];
+            return (string)$_SERVER['HTTP_REFERER'];
         }
         return '/';
     }
