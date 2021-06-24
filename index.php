7e1aecd7f1adb083dc51003c3818c41a778c8453
voopsc <voopstudio@gmail.com>
2021-06-24 15:07:49 +0300

    fix(index.php): some codestyle fix

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 
-    // General settings
-    ini_set('display_errors', '1');
-    define('ROOT', __DIR__);
-    define('FRAMEWORK', ROOT . DIRECTORY_SEPARATOR . 'Framework');
-
-    // Namespaces
 use Framework\Helper\Help;
 use Framework\Router\Router;
-    use Framework\Session\Session;
+use Framework\Session\Session;
 
-    // Autoloader
-    $autoloadPath = [ROOT, 'vendor', "autoload.php"];
-    require_once (implode(DIRECTORY_SEPARATOR, $autoloadPath));
+// General settings
+ini_set('display_errors', '1');
 
-    // Session
-    $session = new Session();
-    $session->start();
+define('ROOT', __DIR__);
+define('FRAMEWORK', ROOT . DIRECTORY_SEPARATOR . 'Framework');
 
-    // Router
-    $routes = Help::getFilepathString([ROOT, 'App', 'config', 'routes.php']);
-    $controller = Help::getFilepathString([ROOT, 'App', 'controller']);
+// Autoloader
+$autoloadPath = [ROOT, 'vendor', "autoload.php"];
+$autoloader = require_once(implode(DIRECTORY_SEPARATOR, $autoloadPath));
 
-    $router = new Router($routes, $controller);
-    $router->run();
+// Session
+$session = new Session();
+$session->start();
+
+// Router
+$routes = Help::getFilepathString([ROOT, 'App', 'config', 'routes.php']);
+$controller = Help::getFilepathString([ROOT, 'App', 'controller']);
+
+$router = new Router($routes, $controller);
+$router->run();
